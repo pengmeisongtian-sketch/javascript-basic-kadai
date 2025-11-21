a770913da958cff450266eccb2a86f93785ebcb4
pengmeisongtian-sketch <pengmeisongtian@gmail.com>
2025-11-21 18:53:55 +0900

    Simplify button click event handling

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -3,11 +3,5 @@ const btn = document.getElementById('btn');
 const text = document.getElementById('text');
 
 btn.addEventListener('click', () => {
-    const childList = document.createElement('li');
-
-    childList.textContent = 'ボタンをクリックしました';
-
-    text.appendChild(childList);
-
     text.textContent = 'ボタンをクリックしました';
 });
